feadef55444e1c8bdc8592668861b1a758cade05
Kenton Olsen <kenton682@gmail.com>
2020-11-05 19:04:26 -0800

    everthing up but needs improvements

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from "react";
 import "./App.css";
 import Row from "./components/Rows/Row.js";
 import requests from "./requests.js";
+import Banner from "./components/Banner/Banner.js";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
-      <div className="App">
+      <div className="app">
+        <Navbar />
+        <Banner />
         <Row
           title="NETFLIX ORIGINALS"
           fetchUrl={requests.fetchNetflixOriginals}
+          isLargeRow
         />
         <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
         <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
--- src/components/Rows/Row.js
+++ src/components/Rows/Row.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "../../axios";
-import "./Rows.css";
+import Youtube from "react-youtube";
+import movieTrailer from "movie-trailer";
+import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-const Row = ({ title, fetchUrl }) => {
+const Row = ({ title, fetchUrl, isLargeRow }) => {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     async function fetchData() {
@@ -16,7 +19,27 @@ const Row = ({ title, fetchUrl }) => {
     fetchData();
   }, [fetchUrl]);
 
-  console.table(movies);
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      console.log(movie.name);
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
 
   return (
     <div className="row">
@@ -25,12 +48,16 @@ const Row = ({ title, fetchUrl }) => {
         {movies.map((movie) => (
           <img
             key={movie.id}
-            className="row_poster"
-            src={`${base_url}${movie.poster_path}`}
+            onClick={() => handleClick(movie)}
+            className={`row_poster ${isLargeRow && `row_posterLarge`}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
             alt={movie.name}
           />
         ))}
       </div>
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 };
